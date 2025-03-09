4d14a4dfe949a7304ad866f3be9d74218f3da7bf
jlvihv <imvihv@gmail.com>
2025-03-09 15:29:40 +0800

    update

--- src/routes/api/parse/+server.ts
+++ src/routes/api/parse/+server.ts
@@ -4,15 +4,6 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 export const POST: RequestHandler = async ({ request }) => {
 	try {
 		const { url } = await request.json();
-
-		// 从 URL 中提取 Pinterest ID
-		// const pinIdMatch = url.match(/\/pin\/([^/]+)/);
-		// if (!pinIdMatch) {
-		// 	return json({ error: '无效的 Pinterest URL' }, { status: 400 });
-		// }
-
-		// const pinId = pinIdMatch[1];
-		// const imageUrl = await getPinImage(pinId, headers.v0);
 		const imageUrl = await getPinImageByUrl(url, headers.v0);
 
 		if (!imageUrl) {
